da98ca39bccce0b8cf9c6e704b1d8bf7f79ae09d
hadil <ht207076@gmail.com>
2024-06-03 22:26:05 +0100

    envirenment modif

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+login() {
+throw new Error('Method not implemented.');
+}
+ username: string ='';
+ password: string='';
+}
+
+
+function login() {
+  throw new Error('Function not implemented.');
+}
